8b50de7a2609da5bd8d8b3104ad4333471ee768e
natmark <natmark0918@gmail.com>
2019-07-15 17:58:28 +0900

    Add ColorSet

---
+++ Sources/XCAssetsKit/ColorSet.swift
@@ -0,0 +1,49 @@
+//
+//  ColorSet.swift
+//  XCAssetsKit
+//
+//  Created by Atsuya Sato on 2019/07/15.
+//
+
+import Foundation
+
+public struct ColorSet {
+    public var contents: ColorSetContents
+}
+
+public struct ColorSetContents {
+    public var info: Info
+    public var colors: [Color]
+    public struct Color {
+        public var idiom: String
+        public var displayGamut: String?
+        public var appearances: [Appearance]?
+        public var color: Color
+
+        private enum CodingKeys: String, CodingKey {
+            case idiom
+            case displayGamut = "display-gamut"
+        }
+        
+        public struct Appearance {
+            public var appearance: String
+            public var value: String
+        }
+        public struct Color {
+            public var colorSpace: String
+            public var components: Components
+
+            private enum CodingKeys: String, CodingKey {
+                case colorSpace = "color-space"
+                case components
+            }
+            
+            public struct Components {
+                public var red: String
+                public var blue: String
+                public var green: String
+                public var alpha: String
+            }
+        }
+    }
+}
