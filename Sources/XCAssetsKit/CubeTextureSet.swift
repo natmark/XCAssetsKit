c13c3661cf450044d6da5feeca310d0016adbdc5
natmark <natmark0918@gmail.com>
2019-07-15 18:32:26 +0900

    Add CubeTextureSet

---
+++ Sources/XCAssetsKit/CubeTextureSet.swift
@@ -0,0 +1,52 @@
+//
+//  CubeTextureSet.swift
+//  XCAssetsKit
+//
+//  Created by Atsuya Sato on 2019/07/15.
+//
+
+public struct CubeTextureSet {
+    public var contents: CubeTextureSetContents
+}
+
+public struct CubeTextureSetContents {
+    public var info: Info
+    public var properties: Properties?
+    public var textures: [Texture]?
+
+    public struct Texture: Decodable {
+        public var cubeFace: String
+        public var idiom: String
+        public var filename: String
+        public var graphicsFeatureSet: String?
+        public var memory: String?
+        public var displayGamut: String?
+        public var scale: String?
+        public var pixelFormat: String?
+        public var unassigned: Bool?
+
+        private enum CodingKeys: String, CodingKey {
+            case cubeFace = "cube-face"
+            case idiom
+            case filename
+            case graphicsFeatureSet = "graphics-feature-set"
+            case memory
+            case displayGamut = "display-gamut"
+            case scale
+            case pixelFormat = "pixel-format"
+            case unassigned = "unassigned"
+        }
+    }
+
+    public struct Properties: Decodable {
+        public var origin: String?
+        public var interpretation: String?
+        public var onDemandResourceTags: [String]?
+
+        private enum CodingKeys: String, CodingKey {
+            case origin
+            case interpretation
+            case onDemandResourceTags = "on-demand-resource-tags"
+        }
+    }
+}
